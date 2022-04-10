d2ff77f61ee65777651523b0b8f0857a4fbe5b9f
liukanshan1024/liukanshan1024 <85719360+liukanshan1@users.noreply.github.com>
2022-04-10 23:33:31 +0800

    Revert "Update Collatz.java"
    This reverts commit 0b9bde69348841686dd78a5e23579fd287746f30.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author CuiYuxin
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0)
-        {
-            return n/2;
-        }
-        else {
-            return 3*n+1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
